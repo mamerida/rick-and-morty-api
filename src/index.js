5bd3127b593c52fd535cc7303085f72c55cf8ca0
mamerida <mamerida2018@gmail.com>
2023-03-08 19:22:44 -0300

    create store and actions file

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from "react-redux";
+import { reducerSample as rootReducer } from "../src/reducer/index";
+import { createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
